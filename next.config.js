df55f2152f363169040b2b1629f3187e2d8f4f33
Jacob Andes <jacob.andes@gmail.com>
2022-06-27 17:00:37 -0700

    refactor: updated images/copy

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   compiler: { styledComponents: true },
-  images: { domains: ['images.unsplash.com'] },
+  images: { domains: ['images.unsplash.com', 'media-exp1.licdn.com'] },
 };
 
 module.exports = nextConfig;
