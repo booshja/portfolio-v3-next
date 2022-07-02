77ff35bf82f49639eb0f19187f9157533ff244b8
Jacob Andes <jacob.andes@gmail.com>
2022-07-01 22:17:20 -0700

    refactor: seo meta tags update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -41,6 +41,9 @@ const MyApp = ({ Component, pageProps }) => {
     <>
       <Head>
         <title>JacobAndes.dev</title>
+        <meta charset="UTF-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta name="generator" content="Next 12.1.6" />
         <meta
           name="description"
           content="Portfolio website for Jacob Andes, Software Engineer from Seattle, WA, USA."
@@ -50,17 +53,16 @@ const MyApp = ({ Component, pageProps }) => {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <meta charset="UTF-8" />
         <meta name="author" content="Jacob Andes" />
-        <meta name="copyright" content="Jacob Andes" />
-        <meta name="generator" content="Next.js" />
-        <meta httpEquiv="content-type" content="text/html; charset=UTF-8" />
         <meta property="og:title" content="Jacob Andes" />
+        <meta property="og:site_name" content="Jacob Andes" />
+        <meta property="og:locale" content="en_US" />
         <meta
           property="og:description"
           content="Portfolio website for Jacob Andes, software engineer from Seattle, WA, USA."
         />
         <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://jacobandes.dev" />
         {/* <meta
           property="og:image"
           content="https://jacobandes.dev/images/og-default.png"
@@ -72,7 +74,9 @@ const MyApp = ({ Component, pageProps }) => {
         {/* <meta property="og:image:width" content="" /> */}
         {/* <meta property="og:image:height" content="" /> */}
         <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content="https://jacobandes.dev" />
         <meta name="twitter:creator" content="@booshja" />
+        <meta name="twitter:site" content="@booshja" />
         <meta name="twitter:title" content="Jacob Andes" />
         <meta
           name="twitter:description"
@@ -82,8 +86,8 @@ const MyApp = ({ Component, pageProps }) => {
           name="twitter:image"
           content="https://jacobandes.dev/images/og-default.png"
         /> */}
-        <link rel="canonical" href="https://jacobandes.dev" />
         <meta name="theme-color" content={currentTheme.bgPrimary} />
+        <link rel="canonical" href="https://jacobandes.dev" />
       </Head>
       <GlobalStyle />
       {/* eslint-disable-next-line */}
