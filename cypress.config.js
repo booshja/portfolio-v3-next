ab2382904fdd9bd8697d3a30ceb3e0fe3d97a880
Jacob Andes <jacob.andes@gmail.com>
2022-11-24 21:41:42 -0800

    chore: loading env's for testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,4 @@
+const { loadEnvConfig } = require('@next/env');
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
@@ -19,6 +20,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // eslint-disable-next-line no-param-reassign
       config.env = process.env;
+      const projectDir = process.cwd();
+      loadEnvConfig(projectDir);
       return config;
     },
     baseUrl: 'http://localhost:3000',
